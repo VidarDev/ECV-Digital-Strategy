a64408c511debbb63d35bd15ac43dc9d096183f4
Tom Engélibert <tomengelibert@MacBook-Pro-de-Tom.local>
2024-11-28 22:35:34 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -192,10 +192,6 @@ const HomePage: React.FC = () => {
           <CarouselNext />
         </Carousel>
       </section>
-
-      <footer className="py-8 bg-gray-800 text-white text-center">
-            <p className="text-sm">© 2024 Location de Chaussures. Tous droits réservés.</p>
-          </footer>
       </main>
   );
 };
